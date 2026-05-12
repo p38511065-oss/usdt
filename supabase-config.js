d6c8b99af952a4ee27beb5e56d4a781f21aea95c
p38511065-oss <p38511065@gmail.com>
2026-05-12 23:40:03 +0530

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,6 @@
+window.SUPABASE_URL = "https://khrkdoealcwgsckxvywv.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_SpEAF0qlUd6ra6tiZD99-w_WLZqmB10";
 window.SUPABASE_CONFIG = {
-  url: "https://khrkdoealcwgsckxvywv.supabase.co",
-  anonKey: "sb_publishable_SpEAF0qlUd6ra6tiZD99-w_WLZqmB10"
+  url: window.SUPABASE_URL,
+  anonKey: window.SUPABASE_ANON_KEY
 };
